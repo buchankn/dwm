88e1b1525a3fdd4edab9753ec996e3892666c9f1
Kevin Buchanan <kevin311@protonmail.com>
2023-02-25 00:22:57 -0800

    Add patch dwm-vanitygaps-6.2.diff

--- config.def.h
+++ config.def.h
@@ -3,6 +3,13 @@
 /* appearance */
 static unsigned int borderpx  = 1;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
+
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char font[]            = "SauceCodePro Nerd Font:size=10";
@@ -39,16 +46,31 @@ static int nmaster     = 1;    /* number of clients in master area */
 static int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
-#include "fibonacci.c"
+#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+#include "vanitygaps.c"
+
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
-	{ "|M|",      centeredmaster },
-	{ ">M>",      centeredfloatingmaster },
-	{ "[@]",      spiral },
-	{ "[\\]",     dwindle },
+	{ "[]=",      tile },    /* Default: Master on left, slaves on right */
+    { "TTT",      bstack },  /* Master on top, slaves on bottom */
+
+    { "[@]",      spiral },  /* Fibonacci spiral */
+    { "[\\]",     dwindle }, /* Decreasing in size right and leftward */
+
+    { "[D]",      deck },    /* Master on left, slaves in monocle-like mode on right */
+    { "[M]",      monocle }, /* All windows on top of each other */
+
+    { "|M|",      centeredmaster },         /* Master in middle, slaves on sides */
+    { ">M>",      centeredfloatingmaster }, /* Same but master floats */
+
+//    { "===",      bstackhoriz },
+//    { "HHH",      grid },
+//    { "###",      nrowgrid },
+//    { "---",      horizgrid },
+//    { ":::",      gaplessgrid },
+
+    { "><>",      NULL },    /* no layout function means floating behavior */
+    { NULL,       NULL },
 };
 
 /* key definitions */
@@ -96,29 +118,37 @@ ResourcePref resources[] = {
 #include "shiftview.c"
 
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_g,      shiftview,      { .i = -1 } },
-	{ MODKEY,			XK_g,      shiftview,      { .i = +1 } },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
-	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
-	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[5]} },
-	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[6]} },
+    /* modifier                     key        function        argument */
+    { MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
+    { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+    { MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
+    { MODKEY,                       XK_b,      togglebar,      {0} },
+    { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+    { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+    { MODKEY,                       XK_o,      incnmaster,     {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_o,      incnmaster,     {.i = -1 } },
+    { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+    { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+    { MODKEY,                       XK_Return, zoom,           {0} },
+
+    { MODKEY,                       XK_z,      incrgaps,       {.i = +3 } },
+    { MODKEY,                       XK_x,      incrgaps,       {.i = -3 } },
+
+    { MODKEY,                       XK_a,      togglegaps,     {0} },
+    { MODKEY|ShiftMask,             XK_a,      defaultgaps,    {0} },
+
+    { MODKEY,                       XK_Tab,    view,           {0} },
+    { MODKEY|ShiftMask,             XK_g,      shiftview,      { .i = -1 } },
+    { MODKEY,                       XK_g,      shiftview,      { .i = +1 } },
+    { MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+    { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, /* tile */
+    { MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[1]} }, /* bstack */
+    { MODKEY,                       XK_y,      setlayout,      {.v = &layouts[2]} }, /* spiral */
+    { MODKEY|ShiftMask,             XK_y,      setlayout,      {.v = &layouts[3]} }, /* dwindle */
+    { MODKEY,                       XK_u,      setlayout,      {.v = &layouts[4]} }, /* deck */
+    { MODKEY|ShiftMask,             XK_u,      setlayout,      {.v = &layouts[5]} }, /* monocle */
+    { MODKEY,                       XK_i,      setlayout,      {.v = &layouts[6]} }, /* centeredmaster */
+    { MODKEY|ShiftMask,             XK_i,      setlayout,      {.v = &layouts[7]} }, /* centeredfloatingmaster */
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_s,      togglesticky,   {0} },
