82d895fb67e035ed6b0dde6d9625d2b92fcf1163
Kevin Buchanan <kevin311@protonmail.com>
2023-02-24 19:52:39 -0800

    Add patch shiftview.c

--- config.def.h
+++ config.def.h
@@ -93,6 +93,8 @@ ResourcePref resources[] = {
         { "mfact",              FLOAT,   &mfact },
 };
 
+#include "shiftview.c"
+
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
@@ -107,6 +109,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY|ShiftMask,             XK_g,      shiftview,      { .i = -1 } },
+	{ MODKEY,			XK_g,      shiftview,      { .i = +1 } },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
